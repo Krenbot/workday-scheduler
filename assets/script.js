b58c04cd061b4191ea6aea6f98fd840e17426a38
Steve Krenn <stevekrenn@gmail.com>
2022-09-30 14:55:50 -0500

    start change of time coloring

--- assets/script.js
+++ assets/script.js
@@ -4,31 +4,31 @@ var currentTime = moment().format("dddd, MMM Do")
 console.log(currentEl)
 
 //Displays current time in header
-currentEl.innerText = currentTime
+currentEl.innerText = currentTime;
 
 //Changes hours to relative colors
-function timeColors() {
-    
-    var currentTime = moment().format(H);
-    var textArea = $('textarea');
+// function timeColors() {
 
-    console.log(currentTime)
+    var current = moment().format(H);
+    var textArea = $('#textarea');
 
-    // if current time > text area = past
-    if (textArea < currentTime) {
-        $(this).addClass('past')
-        // if current time = text area = present
-    } else if (textArea === currentTime) {
-        $(this).addClass('present')
-        // if current time < text area = future
-    } else (textArea > currentTime) {
-        $(this).addClass('future')
-    }
-}
+//     console.log(currentTime)
+
+    // // if current time > text area = past
+    // if (textArea < current) {
+    //     $(this).addClass('past')
+    //     // if current time = text area = present
+    // } else if (textArea === current) {
+    //     $(this).addClass('present')
+    //     // if current time < text area = future
+    // } else (textArea > current) {
+    //     $(this).addClass('future')
+    // }
+
 timeColors();
 
 //btn click listener
-function btnClick() {
-}
+// function btnClick() {
+// }
 
 //save btn save to local storage
