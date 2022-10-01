b8bb2593e9b5eca04d6aa0c77a5f6409cf296b57
Steven Krenn <stevekrenn@gmail.com>
2022-10-01 07:46:25 -0500

    time block hour defenitons working

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,45 @@
 var currentEl = document.getElementById("currentDay");
 var currentTime = moment().format("dddd, MMM Do")
-var textBlock = $(".col-8")
+
+// var textBlock = $(".col-8")
 //Displays current time in header
 currentEl.innerText = currentTime;
 
-// Changes hours to relative colors
 function timeColors() {
-
     var current = moment().hours();
-    console.log(current)
-    
-    $(".time-block").each(function) {
-        var hour = parseInt($(this).attr("id"));
-    }
-    
-    if (hour < current) {
-       
-        $(this).addClass('past')
+    console.log(current);
 
-    } else if (hour === current) {
-        $(this).addClass('present')
-        
-    } else {
-        $(this).addClass('future')
-    }
+    $('.time-block').each(function() {
+        var hourBlock = parseInt($(this).attr('id'));
+        console.log(hourBlock)
+    })
 }
-timeColors();
+
+timeColors()
+
+
+// Changes hours to relative colors
+// 
+
+//     
+//     console.log(current)
+    
+//     $(".time-block").each(function) {
+//         var hour = parseInt($(this).attr("id"));
+//     }
+    
+//     if (hour < current) {
+       
+//         $(this).addClass('past')
+
+//     } else if (hour === current) {
+//         $(this).addClass('present')
+        
+//     } else {
+//         $(this).addClass('future')
+//     }
+// }
+// timeColors();
 
 //btn click listener
 // function btnClick() {
