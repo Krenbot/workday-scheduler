fa58a1bd2e275c3556f81ba518f475f8496430af
Steven Krenn <stevekrenn@gmail.com>
2022-10-01 11:24:53 -0500

    local text entry working, displaying in console

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,17 @@
 var currentEl = document.getElementById("currentDay");
 var currentTime = moment().format("dddd, MMM Do")
-
-// var textBlock = $(".col-8")
+var saveBtnEl = document.querySelectorAll('.saveBtn')
 
 //Displays current time in header
 currentEl.innerText = currentTime;
 
 function timeColors() {
     var current = moment().hours();
-
-    console.log(current);
+    // console.log(current);
 
     $('.time-block').each(function () {
         var hourBlock = parseInt($(this).attr('id'));
-        console.log(hourBlock);
+        // console.log(hourBlock);
 
         //Compares time hour to current and sets class
         if (hourBlock === current) {
@@ -27,8 +25,26 @@ function timeColors() {
 }
 timeColors()
 
-//btn click listener
-// function btnClick() {
-// }
+// //Save input in local storage
+$(".saveBtn").click(function () {
+    var text = $(this).siblings('textarea').val();
+    var time = $(this).parent().attr("id");
+    
+    localStorage.setItem(time, text);
+    // console.log(text)
+});
+
+//Retrives items from local storage on reload
+for (let i = 0; i < hours.length; i++){
+    s$('#' + i).val(localStorage.getItem(i))
+}
+
+//Courtesy of Stack Overflow - 
+// $(document).ready(function () {
+//     $(".btnlocalStorage").on("click", function () {
+//         localStorage.setItem("myContent", $(".TextBoxColors-17").val());
+//         console.log(localStorage.getItem("myContent"));
+//     })
+// });
 
 //save btn save to local storage
