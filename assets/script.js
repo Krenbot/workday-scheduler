40dbcb0a1713a9ddbc818a4fb02bf38ea186754e
Steven Krenn <stevekrenn@gmail.com>
2022-10-01 09:33:07 -0500

    hours work final

--- assets/script.js
+++ assets/script.js
@@ -2,44 +2,30 @@ var currentEl = document.getElementById("currentDay");
 var currentTime = moment().format("dddd, MMM Do")
 
 // var textBlock = $(".col-8")
+
 //Displays current time in header
 currentEl.innerText = currentTime;
 
 function timeColors() {
     var current = moment().hours();
+
     console.log(current);
 
-    $('.time-block').each(function() {
+    $('.time-block').each(function () {
         var hourBlock = parseInt($(this).attr('id'));
-        console.log(hourBlock)
-    })
+        console.log(hourBlock);
+
+        //Compares time hour to current and sets class
+        if (hourBlock === current) {
+            $(this).addClass("present")
+        } else if (hourBlock < current) {
+            $(this).removeClass('past')
+        } else {
+            $(this).addClass('future')
+        }
+    });
 }
-
 timeColors()
-
-
-// Changes hours to relative colors
-// 
-
-//     
-//     console.log(current)
-    
-//     $(".time-block").each(function) {
-//         var hour = parseInt($(this).attr("id"));
-//     }
-    
-//     if (hour < current) {
-       
-//         $(this).addClass('past')
-
-//     } else if (hour === current) {
-//         $(this).addClass('present')
-        
-//     } else {
-//         $(this).addClass('future')
-//     }
-// }
-// timeColors();
 
 //btn click listener
 // function btnClick() {
